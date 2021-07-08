e5959891f2edd8f8a4a63ff31be7c3df78857ff9
Ankit Saini <ankit.saini@nationwide.co.uk>
2021-07-09 00:08:36 +0530

    Add project files.

---
+++ Test POC/Model/Tone.cs
@@ -0,0 +1,15 @@
+﻿using IBM.Cloud.SDK.Core.Authentication.Iam;
+using IBM.Watson.ToneAnalyzer.v3;
+using IBM.Watson.ToneAnalyzer.v3.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Test_POC.Model
+{
+    public class Tone
+    {
+
+    }
+}
